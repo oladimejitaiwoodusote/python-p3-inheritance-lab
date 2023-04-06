2bb103f5257721f2d73c12aadc7733a7e1fb0167
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-04-06 09:28:02 -0400

    python lab

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,10 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
+    def __init__(self,first_name, last_name, knowledge = []):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+
+    def learn(self, taught):
+        self.knowledge.append(taught)
         pass
